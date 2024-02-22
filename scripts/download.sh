3fbe276c7f80291dc03876835aa110e84ce9ae67
RyanSquared <ryan@hashbang.sh>
2024-02-22 17:50:28 -0500

    scripts/download: check tarballs when possible

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,14 +1,42 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# download sources
-rm -rf $PWD/build
+set -euo pipefail
+
+# mkdircd download directory before restricting shell access
 TARGET_DIR=$PWD/build/download
-SVN_URL="releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}"
+mkdir -p "${TARGET_DIR}"
+cd "${TARGET_DIR}"
 
-mkdir -p $TARGET_DIR/webkit
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/Source $TARGET_DIR/webkit/Source
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/Tools $TARGET_DIR/webkit/Tools
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/CMakeLists.txt $TARGET_DIR/webkit/CMakeLists.txt
+set -r
 
-mkdir -p $TARGET_DIR/icu
-curl "https://chromium.googlesource.com/chromium/deps/icu/+archive/${npm_package_config_chromiumICUCommit}.tar.gz" | tar xzf - -C $TARGET_DIR/icu
+# NOTE: do not allow reading passed-in $SHA256SUM, just in case someone does
+# something like `export SHA256SUM=true`.
+if command -v sha256sum; then
+  SHA256SUM=sha256sum
+elif command -v gsha256sum; then
+  SHA256SUM=gsha256sum
+else
+  echo "unable to verify downloaded contents, missing [g]sha256sum"
+  exit 1
+fi
+
+WEBKIT_FILE="webkitgtk-${npm_package_config_webkitGTK}.tar.xz"
+WEBKIT_URL="https://webkitgtk.org/releases/${WEBKIT_FILE}"
+rm "$WEBKIT_FILE" || true
+wget "${WEBKIT_URL}"
+echo "${npm_package_config_webkitGTKIntegrity}  ${WEBKIT_FILE}" | "$SHA256SUM" -c
+tar xf "${WEBKIT_FILE}" "webkitgtk-${npm_package_config_webkitGTK}"
+rm -rf webkit || true
+mv "webkitgtk-${npm_package_config_webkitGTK}" webkit
+
+# NOTE: ICU tarballs are made on the fly and don't produce consistent hashes.
+# Verify the file tree, then verify the files.
+ICU_FILE="${npm_package_config_chromiumICUCommit}.tar.gz"
+ICU_URL="https://chromium.googlesource.com/chromium/deps/icu/+archive/${ICU_FILE}"
+rm "$ICU_FILE" || true
+wget "${ICU_URL}"
+test "$(tar tf "${ICU_FILE}" | sort | "$SHA256SUM" | awk '{ print $1 }')" = "${npm_package_config_chromiumICUTreeIntegrity}"
+rm -rf icu || true
+mkdir icu
+tar xf "${ICU_FILE}" -C icu
+test "$(find icu -type f -exec "$SHA256SUM" {} + | sort | "$SHA256SUM" | awk '{ print $1 }')" = "${npm_package_config_chromiumICUFileIntegrity}"
